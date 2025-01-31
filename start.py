597ddfd37ddb28899ee8505c3a1cf26175be4c6b
Jegzy01 <tomiwajegede29@gmail.com>
2025-01-31 22:37:25 +0100

    changes to datetime

--- start.py
+++ start.py
@@ -16,10 +16,11 @@ app.add_middleware(
 @app.get("/",status_code=status.HTTP_200_OK)
 def home():
     email = "tomiwajegede29@gmail.com"
-    current_time  = datetime.now()
-    github_url = "<https://github.com/JEGZY01/stage-0>"
+    current_time  = str(datetime.now().replace(microsecond=0).isoformat())+"Z"
+    github_url = "https://github.com/JEGZY01/stage-0"
     return{
         "email": email,
         "current_datetime": current_time,
         "github_url": github_url
     }
+
